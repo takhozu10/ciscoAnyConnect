6d38985c57bc8e397c683f4c332488a2b694b96c
takhozu10 <thozumi@biglots.com>
2017-08-02 16:30:46 -0400

    Combined install and configure script

--- anyConnectConfig.sh
+++ anyConnectConfig.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-#Personalize AnyConnect configuration file reside in /Users/$curUser/.anyConnect to reflect current user's information.
+#Install Cisco AnyConnect
+sudo installer -pkg /tmp/AnyConnect.pkg -target / -applyChoiceChangesXML /tmp/CiscoChoices.xml
 
 #Define Current user in a variable
 curUser=`ls -l /dev/console | cut -d " " -f 4`;
 
+#Rename anyconnect file to .anyconnect.
+mv /Users/$curUser/anyconnect /Users/$curUser/.anyconnect;
+
+#Personalize AnyConnect configuration file reside in /Users/$curUser/.anyconnect to reflect current user's information
 #Define Current user's home directory
 curUserDir=`echo /Users/$curUser/.anyconnect`;
 
